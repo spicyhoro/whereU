c8481e08b0d3afb3fc149658b5fea63a17f25564
spicyhoro <spicyhoro777@gmail.com>
2019-11-19 20:21:19 +0900

    admin에 display_list사용

--- here/admin.py
+++ here/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'user_name', 'lnt_lng']
+# Register your models here
