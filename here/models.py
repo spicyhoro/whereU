5d16225164711ce91bcc126777ec0ed599ffc69c
spicyhoro <spicyhoro777@gmail.com>
2019-11-19 20:10:40 +0900

    post에서 템플렛까지 만듬

--- here/models.py
+++ here/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=100, blank=True)
+    lnt_lng = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 # Create your models here.
--- here/views.py
+++ here/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+
+
+
+def post_list(request):
+    post = Post.objects.all()
+
+
+    return render(request, 'here/post_list.html', {'posts':post})
 
 # Create your views here.
