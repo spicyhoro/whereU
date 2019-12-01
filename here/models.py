a6d0709670eac5bc8f5c64177816783faac9d2ab
spicyhoro <spicyhoro777@gmail.com>
2019-12-01 12:53:15 +0900

    이제 만지기 시작

--- here/models.py
+++ here/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    user_name = models.CharField(max_length=100, blank=True)
+    title = models.CharField(max_length=100, verbose_name="제목")
+    user_name = models.CharField(max_length=100, blank=True,
+                                 choices=[1,2,3,4,5,"고양이"])
     lnt_lng = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
