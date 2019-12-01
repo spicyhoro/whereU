7180f8ce1b168d5121a1a20290634ada5b11500d
spicyhoro <spicyhoro777@gmail.com>
2019-12-01 14:03:37 +0900

    model.field 추가

--- here/admin.py
+++ here/admin.py
@@ -4,5 +4,8 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'user_name', 'lnt_lng']
+    list_display = ['id', 'cat', 'title', 'user_name', 'lnt_lng', 'created_at', 'updated_at']
+    list_per_page = 10
+
+
 # Register your models here
--- here/models.py
+++ here/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('h', 'horo'),
+        ('l', 'lolens'),
+        ('s', 'spicyhoro'),
+    )
+
+
     title = models.CharField(max_length=100, verbose_name="제목")
     user_name = models.CharField(max_length=100, blank=True,
-                                 choices=[1,2,3,4,5,"고양이"])
-    lnt_lng = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+                                 choices=STATUS_CHOICES, verbose_name="유저이름")
+    cat = models.CharField(max_length=100, blank=True, verbose_name="고양이")
+    lnt_lng = models.CharField(max_length=100, verbose_name="위치")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="만든날짜")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="최근수정날짜")
 
 
 # Create your models here.
